eadbeb845f7a15bb64cde02804c01c124a628515
Relisiuol <49282232+relisiuol@users.noreply.github.com>
2022-09-19 09:02:31 +0200

    Update CandleStickChartRenderer.swift
    Fix candle stick chart.

--- Source/Charts/Renderers/CandleStickChartRenderer.swift
+++ Source/Charts/Renderers/CandleStickChartRenderer.swift
@@ -150,14 +150,17 @@ open class CandleStickChartRenderer: LineScatterCandleRadarRenderer
                 context.strokeLineSegments(between: _shadowPoints)
                 
                 // calculate the body
+                // Fix width (or am I missing something?)
                 
-                _bodyRect.origin.x = CGFloat(xPos) - 0.5 + barSpace
+                let minWidth = max(((UIScreen.main.bounds.width / 1.5) / CGFloat(max((dataProvider.data?.entryCount ?? 1), 1))), ((CGFloat(xPos) + 0.5 - barSpace) - CGFloat(xPos) - 0.5 + barSpace))
+                _bodyRect.origin.x = _bodyRect.origin.x - (minWidth - _bodyRect.size.wid / 2)
                 _bodyRect.origin.y = CGFloat(close * phaseY)
-                _bodyRect.size.width = (CGFloat(xPos) + 0.5 - barSpace) - _bodyRect.origin.x
+                _bodyRect.size.width = minWidth
                 _bodyRect.size.height = CGFloat(open * phaseY) - _bodyRect.origin.y
                 
                 trans.rectValueToPixel(&_bodyRect)
-                
+
+
                 // draw body differently for increasing and decreasing entry
 
                 if open > close
